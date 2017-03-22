5ceb5561b0ab1133f9403bbac7e577943bcbcedc
Web Dev <super.crazyboy429@yandex.com>
2017-03-22 14:15:43 -0700

    Change first

--- process/js/app.js
+++ process/js/app.js
@@ -1,117 +1,35 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var _ = require('lodash');
-
-var AptList = require('./AptList');
-var AddAppointment = require('./AddAppointment');
-var SearchAppointments = require('./SearchAppointments');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      aptBodyVisible: false,
-      orderBy: 'petName',
-      orderDir: 'asc',
-      queryText: '',
-      myAppointments: []
+      title: 'Appointments',
+      show: true
     } //return
   }, //getInitialState
 
-  componentDidMount: function() {
-    this.serverRequest = $.get('./js/data.json', function(result) {
-      var tempApts = result;
-      this.setState({
-        myAppointments: tempApts
-      }); //setState
-    }.bind(this));
-  }, //componentDidMount
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  }, //componentWillUnmount
-
-  deleteMessage: function(item) {
-    var allApts = this.state.myAppointments;
-    var newApts = _.without(allApts, item);
-    this.setState({
-      myAppointments: newApts
-    }); //setState
-  }, //deleteMessage
-
-  toggleAddDisplay: function() {
-    var tempVisibility = !this.state.aptBodyVisible;
-    this.setState({
-      aptBodyVisible: tempVisibility
-    }); //setState
-  }, //toggleAddDisplay
-
-  addItem: function(tempItem) {
-    var tempApts = this.state.myAppointments;
-    tempApts.push(tempItem);
-    this.setState({
-      myAppointments: tempApts
-    }); //setState
-  }, //addItem
-
-  reOrder: function(orderBy, orderDir) {
-    this.setState({
-      orderBy: orderBy,
-      orderDir: orderDir
-    }); //setState
-  }, //reOrder
-
-  searchApts(q) {
-    this.setState({
-      queryText: q
-    }); //setState
-  }, //searchApts
-
   render: function() {
-    var filteredApts = [];
-    var orderBy = this.state.orderBy;
-    var orderDir = this.state.orderDir;
-    var queryText = this.state.queryText;
-    var myAppointments = this.state.myAppointments; 
+    var showTitle;
+    if (this.state.show) {
+      showTitle = 'New';
+    }
 
-    myAppointments.forEach(function(item) {
-      if(
-        (item.petName.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.ownerName.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.aptDate.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.aptNotes.toLowerCase().indexOf(queryText)!=-1)
-      ) {
-        filteredApts.push(item);
-      }
-    }); //forEach
+    var displayList = {
+      display: this.state.show ? 'block':'none',
+      color: 'red'
+    }
 
-    filteredApts = _.orderBy(filteredApts, function(item) {
-      return item[orderBy].toLowerCase();
-    }, orderDir);//orderBy
-
-    filteredApts = filteredApts.map(function(item, index) {
-      return(
-        <AptList key = { index }
-          singleItem = { item }
-          whichItem = { item }
-          onDelete = { this.deleteMessage } />
-      ) //return
-    }.bind(this)); //filteredApts.map
     return (
       <div className="interface">
-        <AddAppointment
-          bodyVisible = { this.state.aptBodyVisible }
-          handleToggle = { this.toggleAddDisplay }
-          addApt = { this.addItem }
-        />
-        <SearchAppointments
-          orderBy = { this.state.orderBy }
-          orderDir = { this.state.orderDir }
-          onReOrder = { this.reOrder }
-          onSearch = { this.searchApts }
-        />
-        <ul className="item-list media-list">{filteredApts}</ul>
+        <h1>{ showTitle } {this.state.title}</h1>
+        <ul style={displayList}>
+          <li>Buffy 3:30 PM</li>
+          <li>Spot 8:30 PM</li>
+          <li>Goldie 3:50 PM</li>
+        </ul>
       </div>
-    ) //return
+    )
   } //render
 }); //MainInterface
 
